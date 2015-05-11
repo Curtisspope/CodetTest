639f975fc4041124acaab27c18a6c2287f606199
administrator <administrator@lap05031.enterprise.stanfordmed.org>
2015-05-11 11:00:17 -0700

    Initial Commit

---
+++ CodeTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CodeTest
+//
+//  Created by administrator on 5/11/15.
+//  Copyright (c) 2015 Curtiss Pope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
